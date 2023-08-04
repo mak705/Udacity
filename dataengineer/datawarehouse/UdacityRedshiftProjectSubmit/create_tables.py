f7200c6db8cfc7e3d817a2c46524a603119aabbd
Makbul Hussain <makbul.hussain@mareana.com>
2023-08-04 09:30:00 +0530

    project

--- dataengineer/datawarehouse/UdacityRedshiftProjectSubmit/create_tables.py
+++ dataengineer/datawarehouse/UdacityRedshiftProjectSubmit/create_tables.py
@@ -3,19 +3,21 @@ import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
 
-"""drop database tables from drop_table_queries, 
+
+def drop_tables(cur, conn):
+    """drop database tables from drop_table_queries, 
 a list with DROP statements
 """
-def drop_tables(cur, conn):
     for query in drop_table_queries:
         print('Executing drop: '+query)
         cur.execute(query)
         conn.commit()
 
-""" create database tables from create_table_queries, 
+
+def create_tables(cur, conn):
+    """ create database tables from create_table_queries, 
 a list with INSERT statements
 """
-def create_tables(cur, conn):
     for query in create_table_queries:
         print('Executing create: '+query)
         cur.execute(query)
@@ -23,6 +25,7 @@ def create_tables(cur, conn):
 
 
 def main():
+    """Main function to drop tables and create tables"""
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
