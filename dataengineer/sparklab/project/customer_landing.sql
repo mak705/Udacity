b42886bf41f74b882a9414594315eddbcf5d468d
Makbul Hussain <makbul.hussain@mareana.com>
2023-08-12 22:22:02 +0530

    spark

---
+++ dataengineer/sparklab/project/customer_landing.sql
@@ -0,0 +1,28 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `project`.`customer_landing` (
+  `customername` string,
+  `email` string,
+  `phone` string,
+  `birthday` string,
+  `serialnumber` string,
+  `registrationdate` bigint,
+  `lastupdatedate` bigint,
+  `sharewithresearchasofdate` bigint,
+  `sharewithfriendsasofdate` bigint,
+  `sharewithpublicasofdate` bigint
+)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'case.insensitive'='TRUE', 
+  'dots.in.keys'='FALSE', 
+  'ignore.malformed.json'='FALSE', 
+  'mapping'='TRUE') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://makbul-unique/customer_landing'
+TBLPROPERTIES (
+  'classification'='json', 
+  'transient_lastDdlTime'=1691778600'')
